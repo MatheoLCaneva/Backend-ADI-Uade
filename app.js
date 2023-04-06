6969329c741261aef1effd186423c55f1a00d593
Matheo Caneva <matheocaneva@gmail.com>
2023-04-06 19:55:12 -0300

    Carga de archivos

---
+++ app.js
@@ -0,0 +1,79 @@
+//Express
+let express = require('express');
+let cookieParser = require('cookie-parser');
+let bluebird = require('bluebird');
+//incorporo cors
+let cors = require('cors');
+
+//importo router
+let indexRouter = require('./routes/index');
+// let apiRouter = require('./routes/user.route'); //Custom
+// let utilRouter = require('./routes/utils');
+// let classRouter = require('./routes/class.route')
+// let commentRouter = require('./routes/comment.route')
+// let contactRouter = require('./routes/contact.route')
+
+//instancio el servidor
+let app = express();
+
+//engine que permite renderizar paginas web
+app.set('view engine', 'jade');
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+
+//aplico cors
+app.use(cors());
+app.use(cookieParser());
+
+//Indico las rutas de los endpoint
+// app.use('/users', apiRouter);
+app.use('/', indexRouter);
+// app.use('/utils/',utilRouter);
+// app.use('/classes', classRouter)
+// app.use('/comments', commentRouter)
+// app.use('/contacts', contactRouter)
+
+
+require('./config').config();
+
+
+//Database connection --
+let mongoose = require('mongoose')
+mongoose.Promise = bluebird;
+let url = `${process.env.DATABASE1}${process.env.DATABASE2}=${process.env.DATABASE3}=${process.env.DATABASE4}`
+console.log("BD", url);
+let opts = {
+  useNewUrlParser: true,
+  connectTimeoutMS: 20000,
+  useUnifiedTopology: true
+};
+
+mongoose.connect(url, opts)
+  .then(() => {
+    console.log(`Succesfully Connected to theMongodb Database..`)
+  })
+  .catch((e) => {
+    console.log(`Error Connecting to the Mongodb Database...`)
+    console.log(e)
+  })
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+});
+
+
+// Setup server port
+let port = process.env.PORT || 8080;
+// Escuchar en el puerto
+app.listen(port, () => {
+  console.log('Servidor de ABM Users iniciado en el puerto ', port);
+});
+
+
+module.exports = app;
