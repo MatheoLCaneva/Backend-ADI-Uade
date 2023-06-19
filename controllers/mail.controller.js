839f8dc9cf57871eae3b566eeb8befafa2fc7a20
Matheo Caneva <matheocaneva@gmail.com>
2023-06-19 19:26:31 -0300

    Update mail.controller.js

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -21,59 +21,70 @@ exports.sendEmail = async function (req, res, next) {
     let mailOptions = {}
 
     if (req.body.tipo == 1) {
+        const code = Math.floor(100000 + Math.random() * 900000).toString();
         mailOptions = {
             from: 'infoaditest@gmail.com',
             to: req.body.email,
             subject: req.body.asunto,
-            html: `<h1>Estimado/a </br> hola </h4>`,
+            html: `<h1>Estimado/a</h1> </br> <h4>Su codigo de registro es: ${code}. Bienvenido </h4>`,
         };
-    }
-    // else if (req.body.obj.tipo == 0) {
-    //     var mailOptions = {
-    //         from: 'infoapiprofesor@gmail.com',
-    //         to: req.body.obj.email,
-    //         subject: req.body.obj.asunto,
-    //         html: `<h1>Estimado/a ${req.body.obj.name} ${req.body.obj.apellido}</h1> </br> <h3> ${req.body.obj.mensaje}</h3>`,
 
-    //     };
-    // }
-    // else if (req.body.obj.tipo == 2) {
-    //     var mailOptions = {
-    //         from: 'infoapiprofesor@gmail.com',
-    //         to: req.body.obj.email,
-    //         subject: req.body.obj.asunto,
-    //         html: `<h1>Estimado/a </h1> </br> <h3> ${req.body.obj.mensaje}</h3> </br> <h4>Motivo: ${req.body.obj.textoRechazo} </h4>`,
+        try {
+            let info = await transporter.sendMail(mailOptions);
+            console.log("Message sent: %s", info.messageId);
+            return res.status(200).json({ status: 200, message: "Código enviado con éxito", code: code})
+        }
+        catch (error) {
+            console.log("Error envio mail: ", error);
+        }
+    };
+}
+// else if (req.body.obj.tipo == 0) {
+//     var mailOptions = {
+//         from: 'infoapiprofesor@gmail.com',
+//         to: req.body.obj.email,
+//         subject: req.body.obj.asunto,
+//         html: `<h1>Estimado/a ${req.body.obj.name} ${req.body.obj.apellido}</h1> </br> <h3> ${req.body.obj.mensaje}</h3>`,
 
-    //     };
-    // }
+//     };
+// }
+// else if (req.body.obj.tipo == 2) {
+//     var mailOptions = {
+//         from: 'infoapiprofesor@gmail.com',
+//         to: req.body.obj.email,
+//         subject: req.body.obj.asunto,
+//         html: `<h1>Estimado/a </h1> </br> <h3> ${req.body.obj.mensaje}</h3> </br> <h4>Motivo: ${req.body.obj.textoRechazo} </h4>`,
 
-    // else if (req.body.obj.tipo == 3) {
-    //     var mailOptions = {
-    //         from: 'infoapiprofesor@gmail.com',
-    //         to: req.body.obj.email,
-    //         subject: req.body.obj.asunto,
-    //         html: `<h1>Estimado/a </h1> </br> <h3> ${req.body.obj.mensaje}</h3>`,
+//     };
+// }
 
-    //     };
-    // }
+// else if (req.body.obj.tipo == 3) {
+//     var mailOptions = {
+//         from: 'infoapiprofesor@gmail.com',
+//         to: req.body.obj.email,
+//         subject: req.body.obj.asunto,
+//         html: `<h1>Estimado/a </h1> </br> <h3> ${req.body.obj.mensaje}</h3>`,
 
-    // else if (req.body.obj.tipo == 4) {
-    //     var mailOptions = {
-    //         from: 'infoapiprofesor@gmail.com',
-    //         to: req.body.obj.email,
-    //         subject: req.body.obj.asunto,
-    //         html: `<h1>Estimado/a </h1> </br> <h3> ${req.body.obj.mensaje}</h3>  </br> <a>http://localhost:3000/nuevacontrasena/${req.body.obj.email}</a>`,
+//     };
+// }
 
-    //     };
-    // }
+// else if (req.body.obj.tipo == 4) {
+//     var mailOptions = {
+//         from: 'infoapiprofesor@gmail.com',
+//         to: req.body.obj.email,
+//         subject: req.body.obj.asunto,
+//         html: `<h1>Estimado/a </h1> </br> <h3> ${req.body.obj.mensaje}</h3>  </br> <a>http://localhost:3000/nuevacontrasena/${req.body.obj.email}</a>`,
+
+//     };
+// }
 
 
-    try {
-        let info = await transporter.sendMail(mailOptions);
-        console.log("Message sent: %s", info.messageId);
-        return res.status(200).json({ status: 200, message: "Email enviado con éxito" })
-    }
-    catch (error) {
-        console.log("Error envio mail: ", error);
-    }
-};
+// try {
+//     let info = await transporter.sendMail(mailOptions);
+//     console.log("Message sent: %s", info.messageId);
+//     return res.status(200).json({ status: 200, message: "Email enviado con éxito" })
+// }
+// catch (error) {
+//     console.log("Error envio mail: ", error);
+// }
+// };
